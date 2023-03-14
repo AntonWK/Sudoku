28f003d9068fc8ca372681884c663413d1b4c7cc
AntonWK <67833217+AntonWK@users.noreply.github.com>
2023-03-14 16:00:24 +0100

    Add files via upload

---
+++ Sudoku/src/sudoku/GamePanel.java
@@ -0,0 +1,12 @@
+package sudoku;
+
+import java.awt.Dimension;
+import java.awt.Panel;
+
+class GamePanel extends Panel{
+        
+     
+     public GamePanel(){
+        setPreferredSize(new Dimension(800, 800));
+     }
+}
